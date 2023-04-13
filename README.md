220be1cd02f2a796513ae93ff474b414af597d02
Trevor McCasland <trevormccasland1@gmail.com>
2023-04-13 18:40:07 -0500

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+# pet-finder
+An app to help you find a breed of cat or dog to your liking
---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react'
+
+interface iHome {
+
+}
+
+const Home: FC<iHome> = () => {
+    return <h1>Pet Finder</h1>
+}
+
+export default Home;
