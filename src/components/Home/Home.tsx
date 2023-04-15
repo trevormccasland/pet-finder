2389e4fd8c97ccb20ec49d0c335425c6878d24be
Trevor McCasland <trevormccasland1@gmail.com>
2023-04-15 09:08:03 -0500

    add mui and SettingsDialog

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,11 +1,41 @@
-import React, { FC } from 'react'
+import React, { FC, useCallback, useMemo, useState } from 'react'
+import { Box, Paper, Typography } from '@mui/material'
+import TopAppBar from './TopAppBar'
+import './Home.css'
+import { Settings } from './SettingsDialog/SettingsDialog'
+import SettingsDialog from './SettingsDialog'
 
 interface iHome {
 
 }
 
 const Home: FC<iHome> = () => {
-    return <h1>Pet Finder</h1>
+    const [settings, setSettings] = useState<Settings>({
+        dogs: false,
+        cats: false
+    })
+    const [openSettingsDialong, setOpenSettingsDialog] = useState(false)
+    const onSettingsClick = useCallback(() => setOpenSettingsDialog(true), [])
+    const noSettings = !settings.dogs && !settings.cats
+    if (noSettings) {
+        return <Box className='homeContainer'>
+            <TopAppBar onSettingsClick={onSettingsClick} />
+            <Paper>
+                <Typography sx={{marginTop: '24px', paddingTop: '8px', marginLeft: '16px'}} variant='h4'>
+                    No Settings detected
+                </Typography>
+                <Typography sx={{marginTop: '24px', marginLeft: '16px', paddingBottom: '16px'}} variant='body1'>
+                    Get started by clicking on the account circle and select some settings!
+                </Typography>
+            </Paper>
+            <SettingsDialog open={openSettingsDialong} setOpen={setOpenSettingsDialog} settings={settings} setSettings={setSettings} />
+        </Box>
+    }
+    return <Box className='homeContainer'>
+        <TopAppBar onSettingsClick={onSettingsClick} />
+        <Typography variant='body1'>Showing {settings.dogs ? 'dogs' : ''} {settings.dogs && settings.cats ? 'and' : ''} {settings.cats ? 'cats': '' }</Typography>
+        <SettingsDialog open={openSettingsDialong} setOpen={setOpenSettingsDialog} settings={settings} setSettings={setSettings} />
+    </Box>
 }
 
-export default Home;
+export default Home
---
+++ src/components/Home/TopAppBar/TopAppBar.tsx
@@ -0,0 +1,58 @@
+import React, { FC, useCallback, useState } from 'react'
+import { AppBar, IconButton, Menu, MenuItem, Toolbar, Typography } from '@mui/material'
+import { AccountCircle } from '@mui/icons-material'
+import './TopAppBar.css'
+
+interface iTopAppBar {
+    onSettingsClick: () => void 
+}
+
+const TopAppBar: FC<iTopAppBar> = ({ onSettingsClick }) => {
+    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
+    const handleClose = useCallback(() => setAnchorEl(null), [])
+    const handleSettingsClick = useCallback(() => {
+        handleClose()
+        onSettingsClick()
+    },[])
+    const handleMenu = useCallback((event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => setAnchorEl(event.currentTarget), [])
+    return <AppBar>
+        <Toolbar>
+            <Typography  variant='h6'>
+                Pet Finder
+            </Typography>
+            <div className='accountCircle'>
+                <IconButton
+                    href=""
+                    size="large"
+                    aria-label="account of current user"
+                    aria-controls="menu-appbar"
+                    aria-haspopup="true"
+                    onClick={handleMenu}
+                    color="inherit"
+                >
+                    <AccountCircle />
+                </IconButton>
+                <Menu
+                    id="menu-appbar"
+                    anchorEl={anchorEl}
+                    anchorOrigin={{
+                        vertical: 'top',
+                        horizontal: 'right',
+                    }}
+                    keepMounted
+                    transformOrigin={{
+                        vertical: 'top',
+                        horizontal: 'right',
+                    }}
+                    open={Boolean(anchorEl)}
+                    onClose={handleClose}
+                >
+                    <MenuItem onClick={handleClose}>My account</MenuItem>
+                    <MenuItem onClick={handleSettingsClick}>Settings</MenuItem>
+                </Menu>
+            </div>
+        </Toolbar>
+    </AppBar>
+}
+
+export default TopAppBar
