a5fcbaefeaed2a413e0de7d216a706be27b58fcd
Trevor McCasland <trevormccasland1@gmail.com>
2023-06-03 11:46:35 -0500

    fix random pet and style petresults

--- src/components/Home/PetResults/PetResults.tsx
+++ src/components/Home/PetResults/PetResults.tsx
@@ -10,64 +10,78 @@ interface iPetResults {
 }
 
 const PetResults: FC<iPetResults> = ({cats, dogs}) => {
-    return <Paper>
-        <Typography>Dog (yellow) and Cat (red) %</Typography>
+    const data = [
+        {key: '0', title: 'Dog', value: (dogs.length)/(cats.length + dogs.length), color: '#FCF33B'},
+        {key: '1', title: 'Cat', value: (cats.length)/(cats.length + dogs.length), color: '#FC3B3B'}
+    ]
+    return <Paper className="petResultsContainer">
         <div className="pieChartContainer">
-            <PieChart radius={1} viewBoxSize={[2,2]} center={[1, 1]}  data={[
-                {label: 'Dog', value: (dogs.length)/(cats.length + dogs.length), color: '#FCF33B'},
-                {label: 'Cat', value: (cats.length)/(cats.length + dogs.length), color: '#FC3B3B'}
-            ]}/>
+            <Typography sx={{textAlign: 'center', marginBottom: '8px'}} variant='h4'>{dogs.length} Dog{dogs.length > 1 ? 's' : '' } (yellow) and {cats.length} Cat{cats.length > 1 ? 's' : ''} (red)</Typography>
+            <PieChart 
+                animate
+                radius={1} 
+                viewBoxSize={[2,2]} 
+                center={[1, 1]}  
+                data={data}/>
         </div>
-        <Typography>Cats</Typography>
-        <TableContainer>
-            <Table>
-                <TableHead>
-                    <TableRow>
-                        <TableCell>
-                            Name
-                        </TableCell>
-                        <TableCell>
-                            Temperment
-                        </TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {cats.map(cat => (<TableRow>
-                        <TableCell>
-                            {cat.name}
-                        </TableCell>
-                        <TableCell>
-                            {cat.temperament}
-                        </TableCell>
-                    </TableRow>))}
-                </TableBody>
-            </Table>
-        </TableContainer>
-        <Typography>Dogs</Typography>
-        <TableContainer>
-            <Table>
-                <TableHead>
-                    <TableRow>
-                        <TableCell>
-                            Name
-                        </TableCell>
-                        <TableCell>
-                            Temperment
-                        </TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {dogs.map(dog => (<TableRow>
-                        <TableCell>
-                            {dog.name}
-                        </TableCell>
-                        <TableCell>
-                            {dog.temperament}
-                        </TableCell>
-                    </TableRow>))}
-                </TableBody>
-            </Table>
-        </TableContainer>
+        {cats.length > 0 && (
+            <div className="tableContainer">
+                <Typography sx={{marginLeft: '16px', marginTop: '8px'}} variant='h4'>Cats</Typography>
+                <TableContainer>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>
+                                Name
+                            </TableCell>
+                            <TableCell>
+                                Temperment
+                            </TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {cats.map(cat => (<TableRow>
+                            <TableCell>
+                                {cat.name}
+                            </TableCell>
+                            <TableCell>
+                                {cat.temperament}
+                            </TableCell>
+                        </TableRow>))}
+                    </TableBody>
+                </Table>
+                </TableContainer>
+            </div>
+        )}
+        {dogs.length > 0 && (
+            <div className="tableContainer">
+                <Typography sx={{marginLeft: '16px', marginTop: '8px'}} variant='h4'>Dogs</Typography>
+                <TableContainer>
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>
+                                    Name
+                                </TableCell>
+                                <TableCell>
+                                    Temperment
+                                </TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {dogs.map(dog => (<TableRow>
+                                <TableCell>
+                                    {dog.name}
+                                </TableCell>
+                                <TableCell>
+                                    {dog.temperament}
+                                </TableCell>
+                            </TableRow>))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </div>
+        )}
     </Paper>
 }
 
